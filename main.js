f0c6a6d3c874ed2f1814d3f0890a68ce53dfc8bf
ENJUCI <enjuci1506@gmail.com>
2025-10-07 15:28:33 +0100

    update
    added more info on the filling stations

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 
 const gasStations = [
-  { name: "Shell Station", location: "Lagos", distance: "1.2 km" },
-  { name: "TotalEnergies", location: "Abuja", distance: "2.3 km" },
-  { name: "Oando Fuel", location: "Port Harcourt", distance: "1.1 km" },
-  { name: "NNPC Mega Station", location: "Lagos", distance: "3.5 km" },
-  { name: "MRS Filling Station", location: "Kano", distance: "4.2 km" },
-  { name: "Conoil Station", location: "Abuja", distance: "2.7 km" }
+  { name: "Shell Station", location: "Lagos", distance: "1.2 km", people_cued_in: 15, fuel_currently_in_stock: 10 },
+  { name: "TotalEnergies", location: "Abuja", distance: "2.3 km", people_cued_in: 28, fuel_currently_in_stock: 2 },
+  { name: "Oando Fuel", location: "Port Harcourt", distance: "1.1 km", people_cued_in: 8, fuel_currently_in_stock: 7 },
+  { name: "NNPC Mega Station", location: "Lagos", distance: "3.5 km", people_cued_in: 30, fuel_currently_in_stock: 20 },
+  { name: "MRS Filling Station", location: "Kano", distance: "4.2 km", people_cued_in: 2, fuel_currently_in_stock: 6 },
+  { name: "Conoil Station", location: "Abuja", distance: "2.7 km", people_cued_in: 0, fuel_currently_in_stock: 0 },
+  { name: "MRS Filling Station", location: "Abia", distance: "4.2 km", people_cued_in: 2, fuel_currently_in_stock: 6 },
+  { name: "MRS Filling Station", location: "Adamawa", distance: "4.2 km", people_cued_in: 2, fuel_currently_in_stock: 6 },
+  { name: "MRS Filling Station", location: "Enugu", distance: "4.2 km", people_cued_in: 2, fuel_currently_in_stock: 6 },
+  { name: "MRS Filling Station", location: "Gombe", distance: "4.2 km", people_cued_in: 2, fuel_currently_in_stock: 6 },
+  { name: "MRS Filling Station", location: "Calabar", distance: "4.2 km", people_cued_in: 2, fuel_currently_in_stock: 6 },
+  { name: "MRS Filling Station", location: "Calabar", distance: "4.2 km", people_cued_in: 2, fuel_currently_in_stock: 6 },
+  { name: "MRS Filling Station", location: "Port-Harcourt", distance: "4.2 km", people_cued_in: 2, fuel_currently_in_stock: 6 },
 ];
 
 
